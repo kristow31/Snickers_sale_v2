1ee7f7ebfdd0702f4b7ce54790db452b5a4e1f4d
kristow31 <91132063+kristow31@users.noreply.github.com>
2021-11-22 13:32:05 +0300

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y libxss1 libappindicator1 libindicator7
+sudo apt install -y libxss1 libappindicator1 libindicator7 unzip
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 sudo apt install -y -f
@@ -7,3 +7,8 @@ google-chrome --version
 rm -f google-chrome*.deb
 
 # Search chrome driver: https://chromedriver.storage.googleapis.com/index.html
+
+RESULT=$(google-chrome --version)
+wget https://chromedriver.storage.googleapis.com/${RESULT:14:12}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm -f chromedriver_linux64.zip
